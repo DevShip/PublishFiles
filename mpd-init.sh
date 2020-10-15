92a9e4285a65739e8317116812ea96402a282499
ViC <vicheslav@hotmail.com>
2020-10-15 23:44:53 +1200

    Create mpd-init.sh
    First Init

---
+++ mpd-init.sh
@@ -0,0 +1,25 @@
+#/bin/sh
+
+sudo apt-get install mpd mpc
+sudo alsamixer
+sudo alsamixer store 0
+
+mpc repeat on
+mpc random on
+
+mpc clear
+mpc add https://listen2.myradio24.com/8226
+mpc add http://195.242.219.208:8200/enigma
+
+mkdir /srv/http/command
+cd /srv/http/command
+wget https://raw.githubusercontent.com/DevShip/PublishFiles/main/mpd-watchdog
+chmod +x mpd-watchdog
+cd /usr/lib/systemd/system
+wget https://raw.githubusercontent.com/DevShip/PublishFiles/main/mpd-watchdog.service
+systemctl enable mpd-watchdog
+
+systemctl start mpd-watchdog
+systemctl status mpd-watchdog
+
+mpc current
