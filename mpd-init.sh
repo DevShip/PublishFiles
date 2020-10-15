6a911b4b7bbf366343698b3df34e13f8ba3c9ce6
ViC <vicheslav@hotmail.com>
2020-10-16 00:42:52 +1200

    Update mpd-init.sh

--- mpd-init.sh
+++ mpd-init.sh
@@ -1,5 +1,12 @@
 #/bin/sh
 
+echo • ===================================================================================== •
+# Disable previous presets
+mpc stop && mpc clear > /dev/null
+systemctl disable mpd-watchdog > /dev/null
+
+echo • ===================================================================================== •
+# installing packages
 sudo apt-get --yes --force-yes install -f mpd mpc
 
 echo • ===================================================================================== •
